e8e94eb21dbe03e54d7810d5a5795514567ba096
gariasf <accounts@gariasf.com>
2020-01-14 18:25:46 +0100

    Disable tops endpoint (for now)

--- server/endpoints/tops.ts
+++ server/endpoints/tops.ts
@@ -1,10 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { ApiResponse, PlayerStats, PlayerStatsData } from '../../shared/api-types'
-import { responseOK } from '../api'
-import { query, statzQuery } from '../database'
+import { ApiResponse } from '../../shared/api-types'
+import { responseKO } from '../api'
 
 const defaultCols = ['value', 'world']
 
 export async function getTops(req: NextApiRequest, res: NextApiResponse): Promise<ApiResponse<any>> {
-  return responseOK({})
+  return responseKO({
+    error: 'Method not yet implemented',
+    errorMessage: 'Method not yet implemented',
+    statusCode: 500,
+  })
 }
